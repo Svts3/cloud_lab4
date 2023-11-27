dfdcd6a4facd6606679d762a50b476e00c060744
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2023-11-27 12:50:19 +0200

    updated app scaling policy

--- app_autoscaling.tf
+++ app_autoscaling.tf
@@ -18,9 +18,12 @@ resource "aws_appautoscaling_policy" "ecs_service_auto_scaling_up" {
     adjustment_type         = "ChangeInCapacity"
     cooldown                = 300
     step_adjustment {
-      metric_interval_upper_bound = 100
-      metric_interval_lower_bound = aws_cloudwatch_metric_alarm.scale_up_alarm.threshold
+      metric_interval_lower_bound = 50
       scaling_adjustment          = 1
+    }
+    step_adjustment {
+      metric_interval_upper_bound = 100
+      scaling_adjustment = 1
     }
     
   }
@@ -36,10 +39,12 @@ resource "aws_appautoscaling_policy" "ecs_service_auto_scaling_down" {
     adjustment_type         = "ChangeInCapacity"
     cooldown                = 300
     step_adjustment {
-      metric_interval_upper_bound = aws_cloudwatch_metric_alarm.scale_down_alarm.threshold
       metric_interval_lower_bound = 0
       scaling_adjustment          = -1
     }
-    
+    step_adjustment {
+     metric_interval_upper_bound = 20
+     scaling_adjustment = -1 
+    }    
   }
 }
