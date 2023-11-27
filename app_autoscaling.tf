4beeda75c4d47e7cf9534bcf28a193b772b31f1c
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2023-11-27 12:27:02 +0200

    changed app scaling policies

--- app_autoscaling.tf
+++ app_autoscaling.tf
@@ -18,6 +18,7 @@ resource "aws_appautoscaling_policy" "ecs_service_auto_scaling_up" {
     adjustment_type         = "ChangeInCapacity"
     cooldown                = 300
     step_adjustment {
+      metric_interval_upper_bound = 100
       metric_interval_lower_bound = 50
       scaling_adjustment          = 1
     }
